c9be7f4da88ef9b89c2cb037430a47f2602b6448
antondlr <anton@delaruelle.net>
2023-08-23 11:42:43 +0200

    pull out api endpoint into config; allow override through env var

--- src/api/blockprint.ts
+++ src/api/blockprint.ts
@@ -1,5 +1,8 @@
+import { defaultApiEndpoint } from '../../config';
+const apiEndpoint = process.env.API_ENDPOINT || defaultApiEndpoint;
+
 export const getConfusion = async () => {
-  const res = await fetch('https://api.blockprint.sigp.io/confusion')
+  const res = await fetch(apiEndpoint)
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
