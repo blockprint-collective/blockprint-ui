4f468efe0076e5fa655a7790b84eccefc92d3263
Mavrik <mrricki.m.usmc@gmail.com>
2023-08-23 11:29:46 +0200

    fix: adjust percentage format

--- src/utils/getPositivePredictiveValue.ts
+++ src/utils/getPositivePredictiveValue.ts
@@ -1,10 +1,9 @@
 import {Precision} from '@/types';
-import formatPercentage from '@/utils/formatPercentage';
 
 const getPositivePredictiveValue = (data: Precision) => {
     const { true_positives, false_positives } = data;
 
-    return formatPercentage(Math.round((true_positives / (true_positives + false_positives)) * 100))
+    return Math.round((true_positives / (true_positives + false_positives)) * 100)
 }
 
 export default getPositivePredictiveValue;
