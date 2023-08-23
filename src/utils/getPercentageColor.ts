05d7b34bf2ec70d25cf6aae528e3897dd9940ddc
Mavrik <mrricki.m.usmc@gmail.com>
2023-08-23 12:20:52 +0200

    fix: readjust percentage format

--- src/utils/getPercentageColor.ts
+++ src/utils/getPercentageColor.ts
@@ -1,15 +1,11 @@
 const getPercentageColor = (percentage: number | string) => {
   switch (true) {
-      case (percentage < 75):
-          return 'bg-red-500';
-      case percentage >= 75 && percentage < 95:
-          return 'bg-yellow-500';
       case percentage > 95:
           return 'bg-green-500'
-      case percentage === '-':
-          return 'bg-grey-500'
+      case percentage >= 75 && percentage < 95:
+          return 'bg-yellow-500';
       default:
-          return 'bg-grey-500'
+          return 'bg-red-500'
   }
 }
 
