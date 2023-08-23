737c687f4c0ba0500754084a17a446736f44e64c
Mavrik <mrricki.m.usmc@gmail.com>
2023-08-23 10:06:41 +0200

    feat: added basic overview precision table

--- src/components/PercisionTable/PrecisionTable.tsx
+++ src/components/PercisionTable/PrecisionTable.tsx
@@ -1,15 +1,46 @@
 import { FC } from 'react'
 import { PrecisionResults } from '@/types'
+import getTruePositiveRate from '@/utils/getTruePositiveRate';
+import getTrueNegativeRate from '@/utils/getTrueNegativeRate';
+import getPositivePredictiveValue from '@/utils/getPositivePredictiveValue';
+import getPercentageColor from '@/utils/getPercentageColor';
 
 export interface PrecisionTableProps {
   data: PrecisionResults
 }
 
 const PrecisionTable: FC<PrecisionTableProps> = ({ data }) => {
+    const {clients} = data;
+    const clientNames = Object.keys(clients)
+
   return (
-    <div>
-      <p>{Object.keys(data.clients).join(', ')}</p>
-    </div>
+      <table border="1" className="w-1/2 table-auto w-full">
+          <thead>
+          <tr>
+              <th></th>
+              <th>TPR</th>
+              <th>TNR</th>
+              <th>PPV</th>
+          </tr>
+          </thead>
+          <tbody>
+          {clientNames.map((name, index) => {
+              const data = clients[name]
+              const tpr = getTruePositiveRate(data)
+              const tnr = getTrueNegativeRate(data)
+              const ppv = getPositivePredictiveValue(data)
+
+              return (
+                  <tr key={index}>
+                      <td>{name}</td>
+                      <td className={`text-center p-2 bg-opacity-40 ${getPercentageColor(tpr)}`}>{`${tpr} %`}</td>
+                      <td className={`text-center p-2 bg-opacity-40 ${getPercentageColor(tnr)}`}>{`${tnr} %`}</td>
+                      <td className={`text-center p-2 bg-opacity-40 ${getPercentageColor(ppv)}`}>{`${ppv} %`}</td>
+                  </tr>
+              )
+          })}
+          </tbody>
+      </table>
   )
 }
 
