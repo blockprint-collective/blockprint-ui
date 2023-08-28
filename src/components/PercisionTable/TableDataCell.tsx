e320b99c1c39ed34bb4be5f051a06ad8442849ea
Mavrik <mrricki.m.usmc@gmail.com>
2023-08-28 18:49:11 +0200

    feat: created table cell animations and delay anim props

--- src/components/PercisionTable/TableDataCell.tsx
+++ src/components/PercisionTable/TableDataCell.tsx
@@ -4,11 +4,21 @@ import {FC} from 'react';
 
 export interface TableDataCellProps {
     data: number | string
+    animDelay?: number
 }
 
-const TableDataCell:FC<TableDataCellProps> = ({data}) => {
+const TableDataCell:FC<TableDataCellProps> = ({data, animDelay = 0}) => {
+  const {bg, text} = getPercentageColor(data);
+
   return (
-      <td className={`text-center p-2 bg-opacity-40 border-l border-gray-100 ${getPercentageColor(data)}`}>{formatPercentage(data as number)}</td>
+      <td className="relative border-l border-gray-100">
+          <div style={{animationDelay: `${animDelay}ms`}} className={`h-full w-full absolute t-0 opacity-0 l-0 pointer-event-none animate-fade-in-opacity ${bg}`}/>
+          <div className="p-2 text-center">
+              <p className={`animate-fade-in-color ${text}`}>
+                  {formatPercentage(data as number)}
+              </p>
+          </div>
+      </td>
   )
 }
 
