4aaf554a7cbd7dd7f284e8ed9f7a5fa644254f7a
Mavrik <mrricki.m.usmc@gmail.com>
2023-08-28 18:49:40 +0200

    refactor: return object seperating bg and text classes

--- src/utils/getPercentageColor.ts
+++ src/utils/getPercentageColor.ts
@@ -1,11 +1,11 @@
 const getPercentageColor = (percentage: number | string) => {
   switch (true) {
       case percentage > 95:
-          return 'bg-green-500 text-green-600'
+          return {bg: 'bg-green-500', text: 'text-green-600'}
       case percentage >= 75 && percentage < 95:
-          return 'bg-yellow-500 text-yellow-600';
+          return {bg: 'bg-yellow-500', text: 'text-yellow-600'};
       default:
-          return 'bg-red-500 text-red-600'
+          return {bg: 'bg-red-500', text: 'text-red-600'}
   }
 }
 
