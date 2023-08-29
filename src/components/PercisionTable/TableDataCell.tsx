c80cf4b1f78326184545e321e3aa17cd22a5d653
Mavrik <mrricki.m.usmc@gmail.com>
2023-08-29 12:35:24 +0200

    feat: added table cell color, animation and darkText overrides

--- src/components/PercisionTable/TableDataCell.tsx
+++ src/components/PercisionTable/TableDataCell.tsx
@@ -1,20 +1,23 @@
-import getPercentageColor from '@/utils/getPercentageColor';
 import formatPercentage from '@/utils/formatPercentage';
 import {FC} from 'react';
+import {CellColor} from '@/types';
 
 export interface TableDataCellProps {
     data: number | string
     animDelay?: number
+    cellColor: CellColor
+    animation?: string | undefined
+    isDarkText?: boolean
 }
 
-const TableDataCell:FC<TableDataCellProps> = ({data, animDelay = 0}) => {
-  const {bg, text} = getPercentageColor(data);
+const TableDataCell:FC<TableDataCellProps> = ({data, animDelay, isDarkText, cellColor , animation = 'animate-fade-in-opacity'}) => {
+  const {bg, text, darkText} = cellColor;
 
   return (
       <td className="relative border-l border-gray-100">
-          <div style={{animationDelay: `${animDelay}ms`}} className={`h-full w-full absolute t-0 opacity-0 l-0 pointer-event-none animate-fade-in-opacity ${bg}`}/>
+          <div style={{animationDelay: `${animDelay}ms`}} className={`h-full w-full absolute t-0 opacity-0 l-0 pointer-event-none ${animation} ${bg}`}/>
           <div className="p-2 text-center">
-              <p className={`animate-fade-in-color ${text}`}>
+              <p className={`animate-fade-in-color ${isDarkText ? darkText : text}`}>
                   {formatPercentage(data as number)}
               </p>
           </div>
