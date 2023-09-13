3c59d58495b51c7332742e5e064f5f352bd01b18
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-13 16:44:43 +0200

    feat: added styled bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const data = await getConfusion()
 
   return (
-    <main className='flex h-screen w-screen flex-col items-center justify-center p-24'>
+    <main className='flex bg-dark h-screen w-screen flex-col items-center justify-center p-24'>
       <PrecisionTable data={data} />
     </main>
   )
