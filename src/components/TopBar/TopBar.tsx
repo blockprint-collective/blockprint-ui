204aa7055952f5522da7a616140042351a09ebc5
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-15 00:29:45 +0200

    feat: added bg color to layout and href for icon links

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,4 +1,5 @@
 import Typography from '@/components/Typography/Typography';
+import {DISCORD_URL, GITHUB_URL} from '@/constants';
 
 
 const TopBar = () => {
@@ -9,8 +10,12 @@ const TopBar = () => {
                 <div className="h-full cursor-pointer">
                     <Typography type="text-tiny">Client Resources</Typography>
                 </div>
-                <i className="bi-discord text-light w-5 h-5"/>
-                <i className="bi-github text-light w-5 h-5"/>
+                <a target="_blank" href={GITHUB_URL} rel="noreferrer">
+                    <i className="bi-discord text-light w-5 h-5"/>
+                </a>
+                <a target="_blank" href={DISCORD_URL} rel="noreferrer">
+                    <i className="bi-github text-light w-5 h-5"/>
+                </a>
             </div>
         </div>
     )
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const GITHUB_URL = 'https://github.com/sigp/blockprint'
+export const DISCORD_URL = 'https://discord.gg/Pj6tNRWjeP'
