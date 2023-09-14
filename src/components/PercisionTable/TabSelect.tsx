bc8a9af8b68758957e981889b7514c66c34103d1
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-14 14:48:46 +0200

    fix: use string instead of child prop for tab select component

--- src/components/PercisionTable/TabSelect.tsx
+++ src/components/PercisionTable/TabSelect.tsx
@@ -1,17 +1,17 @@
-import {FC, ReactNode} from 'react';
+import {FC} from 'react';
 import Typography from '@/components/Typography/Typography';
 
 export interface TabSelect {
     onClick: () => void
     isActive: boolean
-    children: ReactNode | ReactNode[]
+    text: string
 }
 
-const TabSelect:FC<TabSelect> = ({onClick, isActive, children}) => {
+const TabSelect:FC<TabSelect> = ({onClick, isActive, text}) => {
   return (
       <div onClick={onClick} className="flex-1 flex items-center space-x-2 mb-1 text-center cursor-pointer">
           <Typography color={!isActive ? 'text-light50' : 'text-light'}>
-              {children}
+              {text}
           </Typography>
           {isActive && <hr className="h-.5 w-4 bg-light"/>}
       </div>
