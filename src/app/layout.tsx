22fe50454c332677dffda0e4a4e76bf032d7a144
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-18 16:15:30 +0200

    fix: import font correctly with nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,43 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 
-const inter = Inter({ subsets: ['latin'] })
+const openSuace = localFont({
+  src: [
+    {
+      path: '../../public/fonts/OpenSauce/OpenSauceOne-Regular.ttf',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: '../../public/fonts/OpenSauce/OpenSauceOne-Bold.ttf',
+      weight: '700',
+      style: 'normal'
+    },
+    {
+      path: '../../public/fonts/OpenSauce/OpenSauceOne-Light.ttf',
+      weight: '300',
+      style: 'normal'
+    }
+  ],
+  variable: '--open-sauce'
+})
+
+const archivo = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Archivo/Archivo-Regular.ttf',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: '../../public/fonts/Archivo/Archivo-Bold.ttf',
+      weight: '700',
+      style: 'normal'
+    },
+  ],
+  variable: '--archivo'
+})
 
 export const metadata: Metadata = {
   title: 'Blockprint',
@@ -13,7 +48,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} bg-dark`}>{children}</body>
+      <body className={`${openSuace.variable} ${archivo.variable} bg-dark`}>{children}</body>
     </html>
   )
 }
