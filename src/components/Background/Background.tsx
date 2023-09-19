652a3c8d4166c45b9ac4900e0f3b598ae75f2505
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-19 20:34:32 +0200

    fix: move image to public folder

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,10 +1,8 @@
-'use client'
 import Image from 'next/image';
-import BackgroundImage from '../../assets/image/blockprint-bg.png';
 
 const Background = () => {
   return (
-      <Image className="absolute left-0 top-0 z-0 h-screen" src={BackgroundImage} alt="SigP"/>
+      <Image priority className="absolute left-0 top-0 z-0 h-screen" fill src="/blockprint-bg.png" alt="SigP"/>
   )
 }
 
