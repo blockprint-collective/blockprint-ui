6f3fc2d8e118a4708794f97526546ae2e67c09ee
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-19 21:06:16 +0200

    fix: hide bg if load error

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,8 +1,17 @@
+'use client'
 import Image from 'next/image';
+import {useState} from 'react';
+import addClassString from '@/utils/addClassString';
 
 const Background = () => {
+  const [isView, setView] = useState(true)
+  const classes = addClassString('absolute left-0 top-0 z-0 h-screen', [
+      !isView && 'hidden'
+  ])
+
+  const hideImage = () => setView(false)
   return (
-      <Image priority className="absolute left-0 top-0 z-0 h-screen" fill src="/blockprint-bg.png" alt="SigP"/>
+      <Image priority onError={hideImage} className={classes} fill src="/blockprint-bg.png" alt="SigP"/>
   )
 }
 
