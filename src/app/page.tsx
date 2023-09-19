6b984224c46b4b92dbe696be1b6e77ebece3490c
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-19 19:54:22 +0200

    feat: fetch diversity data and pass it to table, added background component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
-import { getConfusion } from '@/api/blockprint'
+import {getConfusion, getDiversity} from '@/api/blockprint'
 import PrecisionTable from '@/components/PercisionTable/PrecisionTable'
 import TopBar from '@/components/TopBar/TopBar';
 import Header from '@/components/Header/Header';
 import HowItWorks from '@/components/HowItWorks/HowItWorks';
-import background from '../assets/image/blockprint-bg.png'
-import Image from 'next/image';
 import Footer from '@/components/Footer/Footer';
+import getEpochRange from '@/utils/getEpochRange';
+import Background from '@/components/Background/Background';
+import SigmaPrime from '@/components/SigmaPrime/SigmaPrime';
 
 export default async function Home() {
-  const data = await getConfusion()
+  const confusion = await getConfusion()
+  const {start, end} = getEpochRange()
+  const diversity = await getDiversity(start, end)
 
   return (
     <main className='bg-dark h-screen w-screen'>
-        <Image className="absolute left-0 top-0 z-0 h-screen" src={background} alt=""/>
+        <Background/>
         <div className="relative h-screen w-screen z-10">
             <TopBar/>
             <Header/>
-            <PrecisionTable data={data} />
+            <PrecisionTable diversity={diversity} confusion={confusion} />
+            <div className="lg:hidden x-padding py-12">
+                <SigmaPrime/>
+            </div>
             <HowItWorks/>
             <Footer/>
         </div>
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const GITHUB_URL = 'https://github.com/sigp/blockprint'
 export const DISCORD_URL = 'https://discord.gg/Pj6tNRWjeP'
+export const MAINNET_GENESIS = '1606824023'
