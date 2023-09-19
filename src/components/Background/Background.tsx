f3bd807f5672594365923ebdd9ba053c38263159
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-19 21:22:00 +0200

    fix: use svg instead of png

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -2,16 +2,17 @@
 import Image from 'next/image';
 import {useState} from 'react';
 import addClassString from '@/utils/addClassString';
+import BlockBg from '../../assets/image/block-bg.svg'
 
 const Background = () => {
   const [isView, setView] = useState(true)
-  const classes = addClassString('absolute left-0 top-0 z-0 h-screen', [
+  const classes = addClassString('absolute left-0 top-0 z-0', [
       !isView && 'hidden'
   ])
 
   const hideImage = () => setView(false)
   return (
-      <Image priority onError={hideImage} className={classes} fill src="/blockprint-bg.png" alt="SigP"/>
+      <Image priority onError={hideImage} className={classes} fill src={BlockBg} alt="SigP"/>
   )
 }
 
