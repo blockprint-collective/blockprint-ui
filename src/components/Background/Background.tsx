5066437bce2f3c2190de2fc2d0493965fc4c4377
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-19 20:19:00 +0200

    fix: fix bg image url

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,10 +1,10 @@
 'use client'
 import Image from 'next/image';
-import background from '../../assets/image/blockprint-bg.png';
+import BackgroundImage from '../../assets/image/blockprint-bg.png';
 
 const Background = () => {
   return (
-      <Image className="absolute left-0 top-0 z-0 h-screen" src={background} alt=""/>
+      <Image className="absolute left-0 top-0 z-0 h-screen" src={BackgroundImage} alt=""/>
   )
 }
 
