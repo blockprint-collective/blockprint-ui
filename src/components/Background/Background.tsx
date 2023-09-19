f2a52d80afbb5259f9ee68e635d3d7b370a8cba9
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-19 20:23:22 +0200

    fix bg url

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -4,7 +4,7 @@ import BackgroundImage from '../../assets/image/blockprint-bg.png';
 
 const Background = () => {
   return (
-      <Image className="absolute left-0 top-0 z-0 h-screen" src={BackgroundImage} alt=""/>
+      <Image className="absolute left-0 top-0 z-0 h-screen" src={BackgroundImage} alt="SigP"/>
   )
 }
 
