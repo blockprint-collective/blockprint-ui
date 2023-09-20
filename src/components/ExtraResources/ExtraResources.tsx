4cd6194257d3b2c36a5fc2d13d063ca56a42cfe4
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-20 14:48:53 +0200

    feat: added optional sizes for extra resource icons

--- src/components/ExtraResources/ExtraResources.tsx
+++ src/components/ExtraResources/ExtraResources.tsx
@@ -1,13 +1,20 @@
 import {DISCORD_URL, GITHUB_URL} from '@/constants';
+import {FC} from 'react';
 
-const ExtraResources = () => {
+export interface ExtraResourcesProps {
+    size?: 'md' | 'lg'
+}
+
+const ExtraResources:FC<ExtraResourcesProps> = ({size = 'md'}) => {
+   const iconSizes = size === 'lg' ? 'text-base4' : 'text-base'
+
   return (
       <div className="flex space-x-4">
           <a target="_blank" href={GITHUB_URL} rel="noreferrer">
-              <i className="bi-discord text-light w-5 h-5"/>
+              <i className={`bi-discord text-light ${iconSizes}`}/>
           </a>
           <a target="_blank" href={DISCORD_URL} rel="noreferrer">
-              <i className="bi-github text-light w-5 h-5"/>
+              <i className={`bi-github text-light ${iconSizes}`}/>
           </a>
       </div>
   )
