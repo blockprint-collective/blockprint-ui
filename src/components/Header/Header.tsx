23f95e46e306eb718a553bc66300c9abe267677f
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-26 17:29:05 +0200

    added padding to heading title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Typography from '@/components/Typography/Typography';
 const Header = () => {
   return (
       <div className="w-full py-16 x-padding">
-          <Typography color="text-light80" className="tracking-tighter lg:text-base2" fontWeight="font-light" type="text-base" family="font-archivo">Machine Learning for Ethereum Consensus</Typography>
+          <Typography color="text-light80" className="tracking-tighter lg:text-base2 pb-4" fontWeight="font-light" type="text-base" family="font-archivo">Machine Learning for Ethereum Consensus</Typography>
           <Typography className="max-w-lg lg:max-w-2xl tracking-title leading-9 lg:leading-title lg:text-base5" type="text-base4">
               Blockprint. Shaping the Future of Blockchain Transparency
           </Typography>
