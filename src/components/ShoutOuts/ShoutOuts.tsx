6e5467a2dce4d17b82b67024d4ca0efb430da3e6
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-30 02:06:17 +0200

    feat: added animations to sections

--- src/components/ShoutOuts/ShoutOuts.tsx
+++ src/components/ShoutOuts/ShoutOuts.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image';
 import Typography from '@/components/Typography/Typography';
 import EthF from '../../assets/image/eth-foundation.svg'
@@ -6,28 +8,60 @@ import SigP from '@/assets/image/sigp.svg';
 import MigaLabs from '@/assets/image/miga-labs.svg';
 import Rated from '@/assets/image/rated.svg';
 import Section from '@/components/Section/Section';
+import {motion} from 'framer-motion';
 
 const ShoutOuts = () => {
+    const anim = {
+        viewport: {once: true, amount: .4},
+        initial: { opacity: 0, y: 50},
+        whileInView: { opacity: 1, y: 0},
+    }
+
+    const links = [
+        {url: 'https://ethereum.org/', image: EthF, width: 'w-32'},
+        {url: 'https://clientdiversity.org/', image: ClientD, width: 'w-36'},
+        {url: 'https://sigmaprime.io/', image: SigP, width: 'w-10'},
+        {url: 'https://migalabs.io/', image: MigaLabs, width: 'w-36'},
+        {url: 'https://www.rated.network/?network=mainnet&view=pool&timeWindow=1d&page=1&poolType=all', image: Rated, width: 'w-28'},
+    ]
+
+
   return (
       <Section>
           <div className="w-full pt-12 lg:pt-24 x-padding space-y-8">
-              <Typography color="text-light80">Used By</Typography>
-              <div className="w-full overflow-scroll scrollbar-hide flex space-x-12 items-center">
-                  <a href="https://ethereum.org/" target="_blank" rel="noreferrer">
-                      <Image alt="Ethereum Foundation" className="w-32 max-w-none" src={EthF}/>
-                  </a>
-                  <a href="https://clientdiversity.org/" target="_blank" rel="noreferrer">
-                      <Image alt="Client Diversity" className="w-36 max-w-none" src={ClientD}/>
-                  </a>
-                  <a href="https://sigmaprime.io/" target="_blank" rel="noreferrer">
-                      <Image alt="Sigp" className="w-10 max-w-none" src={SigP}/>
-                  </a>
-                  <a href="https://migalabs.io/" target="_blank" rel="noreferrer">
-                      <Image alt="Miga Labs" className="w-36 max-w-none" src={MigaLabs}/>
-                  </a>
-                  <a href="https://www.rated.network/?network=mainnet&view=pool&timeWindow=1d&page=1&poolType=all" target="_blank" rel="noreferrer">
-                      <Image alt="Rated" className="w-28 max-w-none" src={Rated}/>
-                  </a>
+              <motion.div
+                  initial={anim.initial}
+                  viewport={anim.viewport}
+                  whileInView={anim.whileInView}
+                  transition={{duration: .4}}
+              >
+                  <Typography color="text-light80">Used By</Typography>
+              </motion.div>
+              <motion.div
+                  initial={anim.initial}
+                  viewport={anim.viewport}
+                  whileInView={anim.whileInView}
+                  transition={{duration: .4, delay: .2}}
+                  className="w-full flex overflow-scroll scrollbar-hide lg:hidden space-x-12 items-center">
+                  {links.map(({url, image, width}, index) => (
+                      <a key={index}
+                          href={url} target="_blank" rel="noreferrer">
+                          <Image alt="Ethereum Foundation" className={`${width} max-w-none`} src={image}/>
+                      </a>
+                  ))}
+              </motion.div>
+              <div className="w-full hidden lg:flex space-x-12 items-center">
+                  {links.map(({url, image, width}, index) => (
+                      <motion.a
+                          key={index}
+                          initial={anim.initial}
+                          viewport={anim.viewport}
+                          whileInView={anim.whileInView}
+                          transition={{duration: .4, delay: .3 * index}}
+                          href={url} target="_blank" rel="noreferrer">
+                          <Image alt="Ethereum Foundation" className={`${width} max-w-none`} src={image}/>
+                      </motion.a>
+                  ))}
               </div>
           </div>
       </Section>
