f4c857907eb262323b830f681aa86f7cd8a161ad
antondlr <anton@delaruelle.net>
2023-10-12 10:05:41 +0200

    tune cache policy: 60s instead of no-cache

--- src/api/blockprint.ts
+++ src/api/blockprint.ts
@@ -2,7 +2,7 @@ import { defaultApiEndpoint } from '../../config';
 const apiEndpoint = process.env.API_ENDPOINT || defaultApiEndpoint;
 
 export const getConfusion = async () => {
-  const res = await fetch(apiEndpoint, {cache: 'no-store'})
+  const res = await fetch(apiEndpoint, {next: { revalidate: 60 }})
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
@@ -12,7 +12,7 @@ export const getConfusion = async () => {
 }
 
 export const getDiversity = async (start, end) => {
-  const res = await fetch(`https://api.blockprint.sigp.io/blocks_per_client/${start}/${end}`, {cache: 'no-store'})
+  const res = await fetch(`https://api.blockprint.sigp.io/blocks_per_client/${start}/${end}`, {next: { revalidate: 60 }})
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
